f3ec29fd07c35e6e9bfa02dfc3237e0c6150b6c6
Franck <franckferdinand@yahoo.fr>
2019-01-11 12:52:21 +0100

    modifcations/ ajout boutton design

---
+++ src/listeArticle.php
@@ -0,0 +1,17 @@
+<?php
+include_once "database.php";
+include_once "repository/billetRepository.php";
+
+$id = $_GET['id'];
+
+$db = openDatabase('blog','root','troiswa');
+$db->query('SET NAMES UTF8');
+
+$billet = findBillet($db, $id);
+
+
+$template = "../templates/listeArticle.phtml";
+
+include_once "../templates/base.phtml";
+
+?>
