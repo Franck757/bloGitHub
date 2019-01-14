28e4d45c729d68a5f13e89419f1cf52ed4556c6b
Franck <franckferdinand@yahoo.fr>
2019-01-14 12:56:00 +0100

    version 2.0 / nouvelle interface, page d'acceuil + nav

--- src/repository/billetRepository.php
+++ src/repository/billetRepository.php
@@ -19,72 +19,72 @@ function findAllBillet(PDO $db) : array
 /* création d'un billet */
 function writeArticle(PDO $db, array $data)
 {
- $statement = $db->prepare(
-   'INSERT INTO `billet`(`titre`, `corps_de_texte`, `image`, `auteur`)
+  $statement = $db->prepare(
+    'INSERT INTO `billet`(`titre`, `corps_de_texte`, `image`, `auteur`)
     VALUES(:titre, :texte, :image, :auteur)');
- $err = $statement->execute(array(
-     'titre' => $data["titre"],
-     'texte' => $data["texte"],
-     'image' => $data["image"],
-     'auteur' => $data['auteur']
-     ));
- return $err;
-}
+    $err = $statement->execute(array(
+      'titre' => $data["titre"],
+      'texte' => $data["texte"],
+      'image' => $data["image"],
+      'auteur' => $data['auteur']
+    ));
+    return $err;
+  }
 
 
-function findUser(PDO $db, string $id) : array
-{
-  $statement = $db->prepare("SELECT `nom`, `prenom`, `mot_de_passe`, `pseudo`, `mail` FROM `utilisateurs` WHERE `id` = ?");
-  $err = $statement->execute([$id]);
+  function findUser(PDO $db, string $id) : array
+  {
+    $statement = $db->prepare("SELECT `nom`, `prenom`, `mot_de_passe`, `pseudo`, `mail` FROM `utilisateurs` WHERE `id` = ?");
+    $err = $statement->execute([$id]);
 
-  return $statement->fetchAll(PDO::FETCH_ASSOC);
-}
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+  }
 
 
 
-//création d'un utilisateur
-function newUser(PDO $db, array $data)
-{
- $statement = $db->prepare(
-   'INSERT INTO `utilisateurs`(`nom`, `prenom`, `mot_de_passe`, `pseudo`, `mail`)
-    VALUES(:name, :firstname, :password, :pseudo, :mail)');
- $err = $statement->execute(array(
-     'name'       => $data["name"],
-     'firstname'  => $data["firstname"],
-     'password'   => $data["password"],
-     'pseudo'     => $data["pseudo"],
-     'mail'       => $data["mail"]
-     ));
- return $err;
-}
+  //création d'un utilisateur
+  function newUser(PDO $db, array $data)
+  {
+    $statement = $db->prepare(
+      'INSERT INTO `utilisateurs`(`nom`, `prenom`, `mot_de_passe`, `pseudo`, `mail`)
+      VALUES(:name, :firstname, :password, :pseudo, :mail)');
+      $err = $statement->execute(array(
+        'name'       => $data["name"],   //le premier 'name' correspond a "l'étiquette", la VALUES juste au dessus
+        'firstname'  => $data["firstname"],
+        password_hash('password')   => $data["password"],
+        'pseudo'     => $data["pseudo"],
+        'mail'       => $data["mail"]
+      ));
+      return $err;
+    }
 
-/**
- * Trouver un utilisateur par son pseudo
- *
- * @param  PDO    $db     [description]
- * @param  string $pseudo [description]
- * @return array          [description]
- */
-function findUserByPseudo(PDO $db, string $pseudo)
-{
-  $statement = $db->prepare("SELECT * FROM `utilisateurs` WHERE `pseudo` = ?");
-  $err = $statement->execute([$pseudo]);
+    /**
+    * Trouver un utilisateur par son pseudo
+    *
+    * @param  PDO    $db     [description]
+    * @param  string $pseudo [description]
+    * @return array          [description]
+    */
+    function findUserByPseudo(PDO $db, string $pseudo)
+    {
+      $statement = $db->prepare("SELECT * FROM `utilisateurs` WHERE `pseudo` = ?");
+      $err = $statement->execute([$pseudo]);
 
-  return $statement->fetch(PDO::FETCH_ASSOC);
-}
-/**
- * Trouver un utilisateur par son mot de passe
- *
- * @param  PDO    $db     [description]
- * @param  string $pseudo [description]
- * @return array          [description]
- */
-/*function findUserByMdp(PDO $db, string $pseudo) : array
-{
-  $statement = $db->prepare("SELECT * FROM `utilisateurs` WHERE `mot_de_passe` = ?");
-  $err = $statement->execute([$mdp]);
+      return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+    /**
+    * Trouver un utilisateur par son mot de passe
+    *
+    * @param  PDO    $db     [description]
+    * @param  string $pseudo [description]
+    * @return array          [description]
+    */
+    /*function findUserByMdp(PDO $db, string $pseudo) : array
+    {
+    $statement = $db->prepare("SELECT * FROM `utilisateurs` WHERE `mot_de_passe` = ?");
+    $err = $statement->execute([$mdp]);
 
-  return $statement->fetch(PDO::FETCH_ASSOC);
-}*/
+    return $statement->fetch(PDO::FETCH_ASSOC);
+  }*/
 
- ?>
+  ?>
